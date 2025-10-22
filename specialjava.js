d9329cca7d09bfc99cc997d8f405ddf37ae0e2fd
Matt6oh9 <matheus.scaramal@escola.pr.gov.br>
2025-10-22 14:54:18 +0000

    supernecessarytags

---
+++ specialjava.js
@@ -0,0 +1,11 @@
+// Pure nonsense for the sake of fun
+const h2 = document.querySelector('.flaunty');
+for (let i = 0; i < 25; i++) {
+  const spark = document.createElement('span');
+  spark.classList.add('firework');
+  spark.style.setProperty('--x', `${(Math.random() - 0.5) * 600}px`);
+  spark.style.setProperty('--y', `${(Math.random() - 0.5) * 600}px`);
+  spark.style.background = `hsl(${Math.random() * 360}, 100%, 70%)`;
+  spark.style.animationDelay = `${Math.random() * 0.25}s`;
+  h2.appendChild(spark);
+}
